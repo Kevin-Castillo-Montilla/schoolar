12d27af262736d1db2b82167a725b6fd0b5d9da1
kevin <kevinandrescastillomontilla@gmail.com>
2025-03-28 17:40:14 -0500

    chango

--- src/signup.php
+++ src/signup.php
@@ -5,9 +5,11 @@ $fname  = $_POST['f_name'];
 $lname  = $_POST['l_name'];
 $email  = $_POST['e_mail'];
 $passwd = $_POST['passw'];
+//encriptar 
+$enc_pass = md5($passwd);
 //crear sql 
-$sql = "INSERT INTO users(firstname,lastname,email,password)
-    VALUES('$fname','$lname','$email','$passwd')
+$sql = "INSERT INTO users (firstname,lastname,email,password)
+            VALUES('$fname','$lname','$email','$enc_pass')
 ";
 $res = pg_query($conn, $sql);
 if ($res){
